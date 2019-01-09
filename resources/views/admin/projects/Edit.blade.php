5a353fde238a3a0851c9547b5cef41b2d3b28616
salimsrj <salimsrj@gmail.com>
2019-01-09 14:41:10 +0600

    test

--- resources/views/admin/projects/Edit.blade.php
+++ resources/views/admin/projects/Edit.blade.php
@@ -7,7 +7,6 @@
 <section class="content">
     <div class="row">
     <div class="col-md-12">
-
     <div class="box">
             <div class="box-header with-border">
               <h3 class="box-title">Update Form</h3>
